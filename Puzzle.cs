6cc242347e85f3058203609bd4ed313a7caf423f
Rebmiami <59275598+Rebmiami@users.noreply.github.com>
2021-03-27 22:33:09 -0400

    Puzzle-related classes with properties

---
+++ Puzzle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemporalThievery
+{
+	public class Puzzle
+	{
+		public string Name { get; set; }
+
+		public int CashGoal { get; set; }
+
+		public int MaxTimelines { get; set; }
+
+		public int Jump { get; set; }
+
+		public int Branch { get; set; }
+
+		public int Kill { get; set; }
+
+		public int Return { get; set; }
+
+		public string Theme { get; set; }
+
+		public Timeline[] Timelines { get; set; }
+	}
+}
---
+++ Timeline.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemporalThievery
+{
+	public class Timeline
+	{
+		public Element[] Elements { get; set; }
+
+		public int[,] Layout { get; set; }
+	}
+}
