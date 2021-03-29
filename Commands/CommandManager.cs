2a1dff7ebe9e51b6970341859281e3725a517ddb
Rebmiami <59275598+Rebmiami@users.noreply.github.com>
2021-03-29 12:43:28 -0400

    More summaries

--- Commands/CommandManager.cs
+++ Commands/CommandManager.cs
@@ -6,6 +6,25 @@ namespace TemporalThievery
 {
 	public class CommandManager
 	{
+		/// <summary>
+		/// A stack of all <see cref="ICommand"/>s executed since the current <see cref="Puzzle"/> began.
+		/// </summary>
 		public Stack<ICommand> commands;
+
+		/// <summary>
+		/// Reference to the current <see cref="Puzzle"/> for <see cref="ICommand"/>s to act on.
+		/// </summary>
+		public Puzzle puzzle;
+
+		public void Execute(ICommand command)
+		{
+			command.Execute(puzzle);
+			commands.Push(command);
+		}
+
+		public void Undo()
+		{
+			commands.Pop().Undo(puzzle);
+		}
 	}
 }
--- Commands/ICommand.cs
+++ Commands/ICommand.cs
@@ -4,10 +4,21 @@ using System.Text;
 
 namespace TemporalThievery
 {
+	/// <summary>
+	/// Interface handling the execution of any action the player can take.
+	/// </summary>
 	public interface ICommand
 	{
+		/// <summary>
+		/// Executes the command on a given <see cref="Puzzle"/>.
+		/// </summary>
+		/// <param name="puzzle"></param>
 		public void Execute(Puzzle puzzle);
 
+		/// <summary>
+		/// Undoes the action performed by calling <see cref="Execute(Puzzle)"/>.
+		/// </summary>
+		/// <param name="puzzle"></param>
 		public void Undo(Puzzle puzzle);
 	}
 }
