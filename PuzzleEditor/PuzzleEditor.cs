8a52feb32cd506d61cdd5e58cbec6956f1d0bdb6
Rebmiami <59275598+Rebmiami@users.noreply.github.com>
2023-02-04 07:59:29 -0500

    Added editor class

---
+++ PuzzleEditor/PuzzleEditor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TemporalThievery.PuzzleEditor
+{
+	public class PuzzleEditor
+	{
+		bool editing = true;
+		PuzzleState editingState;
+		PuzzleState testingState;
+
+		/// <summary>
+		/// Loads the puzzle state into the editor.
+		/// </summary>
+		/// <param name="puzzleState"></param>
+		public void Initialize(PuzzleState puzzleState)
+		{
+			editingState = (PuzzleState)puzzleState.Clone();
+		}
+
+		public void Reset()
+		{
+			
+		}
+
+		public void Playtest()
+		{
+
+		}
+	}
+}
